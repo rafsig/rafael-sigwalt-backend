eef2e1030a03f180008ab2c499cb0acf9a2580fb
rafsig <rafsig@hotmail.com>
2024-09-18 15:53:35 -0400

    add more validation on message endpoint

--- src/main/java/com/rafael_sigwalt/personal_website/dtos/PostMessageDTO.java
+++ src/main/java/com/rafael_sigwalt/personal_website/dtos/PostMessageDTO.java
@@ -2,18 +2,25 @@ package com.rafael_sigwalt.personal_website.dtos;
 
 import com.rafael_sigwalt.personal_website.models.Message;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
 
 public class PostMessageDTO {
 
     @NotBlank
+    @Length(max = 200)
     private String message;
 
     @NotBlank
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Invalid email address")
+    @Length(max = 50)
     private String email;
 
     @NotBlank
+    @Length(max = 50)
     private String fullName;
 
+    @Length(max = 50)
     private String company;
 
     public Message getMessage(){
